43d08a1c9c3610f3e21fe347564f5e597e12d4fd
bluejww <bluejww@163.com>
2020-06-01 17:34:13 +0800

    再次提交

---
+++ src/view/shopee.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="shopee">
+        <Form :v-model="form">
+            <FormItem prop='email'>
+                <input v-model="form.email" placeholder="价格">
+                </input>
+            </FormItem>
+        </Form>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
