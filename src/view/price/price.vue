715d19771731ed0305a25806bcb7d7c475678ac0
bluejww <bluejww@163.com>
2020-06-03 17:35:45 +0800

    commit again

--- src/view/price/price.vue
+++ src/view/price/price.vue
@@ -1,5 +1,12 @@
 <template>
-    <Input v-model="value" placeholder="Enter something..." style="width: 300px" />
+    <div class="price">
+        <Card class="price-card" title="欢迎登录" :bordered="false">
+            <Input class="price-ip1" v-model="value" placeholder="Enter something..." />
+        </Card>
+        
+
+    </div>
+    
 </template>
 <script>
     export default {
@@ -10,3 +17,6 @@
         }
     }
 </script>
+<style lang="less">
+    @import './price.less';
+</style>
